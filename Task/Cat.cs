576051a3efae8017d1662c4bee2d02d7b5870f82
Kirill Shlyakov <Kirill Shlyakov@DESKTOP-4PP6PL7>
2022-07-28 00:01:18 +0300

    Добавьте файлы проекта.

---
+++ Task/Cat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Cat : Animal
+    {
+        private string animalname;
+         public override string AnimalName 
+        { get => animalname; 
+          set => animalname=value; 
+        }
+
+        public Cat(string animalname) : base(animalname)
+        {
+
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+}
